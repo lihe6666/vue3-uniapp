2ad0b2960a664a88453f09c74ed5a66033105ef9
李大壮 <lihe6666@163.com>
2023-07-06 13:24:48 +0800

    Update: .gitignore

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,21 @@
 <template>
   <view class="wrapper">
+<<<<<<< HEAD
     <uni-badge text="1">{{ title }}</uni-badge>
+=======
+    <uni-badge text="1"></uni-badge>
+>>>>>>> 530f18a (1. added sass plugins.)
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+<<<<<<< HEAD
+=======
+import { http } from '@/library/http'
+
+console.log(getApp())
+>>>>>>> 530f18a (1. added sass plugins.)
 
 const title = ref('Hello')
 </script>
